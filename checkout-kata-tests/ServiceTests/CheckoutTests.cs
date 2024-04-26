e8af7d75d262af78d0d6fb99e663aa0253f69772
Harry <harry.woodall@stockport.gov.uk>
2024-04-26 12:45:51 +0100

    feat(checkout): basic setup, add dependencies

---
+++ checkout-kata-tests/ServiceTests/CheckoutTests.cs
@@ -0,0 +1,14 @@
+using checkout_kata.Services;
+
+namespace checkout_kata_tests.ServiceTests
+{
+    public class CheckoutTests
+    {
+        Checkout checkout = new Checkout();
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
